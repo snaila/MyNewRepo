a5103f04b5167bbb9dcdbb489c995329f5602a58
Użytkownik <uzytkownik@kursGIT.pl>
2024-03-23 11:26:36 +0100

    Add app to flatten list

---
+++ task_flatten_list.py
@@ -0,0 +1,19 @@
+"""
+Do zrobienia będzie funkcja `flatten_list` która przyjmować będzie listę która jako element może mieć albo kolejną listę,
+albo liczbę całkowitą i w wyniku ma zwrócić wszystkie liczby całkowite bez zagnieżdżonych list, musimy założyć, że nie wiemy ile
+będzie stopni zagnieżdżenia.
+Przykładowa lista: [1, [2, 3, [4, [5, 6], 7]], 8, 9, [[10]]]
+Oczekiwany wynik: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""
+
+
+def flatten_list(lst):
+    result_list = []
+    for el in lst:
+        if isinstance(el, list):
+            result_list.extend(flatten_list(el))
+        else:
+            result_list.append(el)
+    return result_list
+
+print(flatten_list([1, [2, 3, [4, [5, 6], 7]], 8, 9, [[10]]]))
